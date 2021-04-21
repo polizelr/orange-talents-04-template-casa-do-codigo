79153276e7f6f1aca50f71b9618037b3ca500b04
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-04-21 03:41:33 -0300

    Adding AuthorController

--- src/main/java/br/com/zupacademy/rafaela/casadocodigo/Author/AuthorController.java
+++ src/main/java/br/com/zupacademy/rafaela/casadocodigo/Author/AuthorController.java
@@ -1,2 +1,17 @@
-package br.com.zupacademy.rafaela.casadocodigo.Author;public class AuthorController {
+package br.com.zupacademy.rafaela.casadocodigo.Author;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/autor")
+@RestController
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorController(AuthorRepository authorRepository){
+        this.authorRepository = authorRepository;
+    }
 }
