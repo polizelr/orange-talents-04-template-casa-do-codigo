c0017810bf97909a66e8830bf60dc77130fc4706
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-04-21 05:03:12 -0300

    Adding ErrorDTO

--- src/main/java/br/com/zupacademy/rafaela/casadocodigo/config/validation/ErrorDTO.java
+++ src/main/java/br/com/zupacademy/rafaela/casadocodigo/config/validation/ErrorDTO.java
@@ -1,2 +1,19 @@
-package br.com.zupacademy.rafaela.casadocodigo.config.validation;public class ErrorDTO {
+package br.com.zupacademy.rafaela.casadocodigo.config.validation;
+
+public class ErrorDTO {
+    String Field;
+    String Error;
+
+    public ErrorDTO(String field, String error) {
+        Field = field;
+        Error = error;
+    }
+
+    public String getField() {
+        return Field;
+    }
+
+    public String getError() {
+        return Error;
+    }
 }
