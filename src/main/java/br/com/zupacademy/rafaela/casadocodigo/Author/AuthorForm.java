f6924df0bc31abcab0a7a5d98835560900d0f8a3
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-04-21 04:19:59 -0300

    Adding AuthorForm DTO

--- src/main/java/br/com/zupacademy/rafaela/casadocodigo/Author/AuthorForm.java
+++ src/main/java/br/com/zupacademy/rafaela/casadocodigo/Author/AuthorForm.java
@@ -1,2 +1,39 @@
-package br.com.zupacademy.rafaela.casadocodigo.Author;public class AuthorForm {
+package br.com.zupacademy.rafaela.casadocodigo.Author;
+
+
+import javax.validation.constraints.*;
+
+public class AuthorForm {
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    private String Name;
+
+    @Email
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    private String Email;
+
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Size(max = 400)
+    private String Description;
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public Author convert(){
+        return new Author(Name, Email, Description);
+    }
 }
